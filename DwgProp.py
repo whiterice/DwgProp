6f6dfd0ea2b3f5ff6629d65eb3a467c279b36c24
Scott Vermeire <svermeire@powercoreeng.com>
2015-05-05 10:20:44 -0400

    finished

--- DwgProp.py
+++ DwgProp.py
@@ -1,10 +1,12 @@
 import sys
 import os.path
-import re
 import argparse
 import csv
+import datetime as DT
 
 DWG_List=[]
+Heading=[]
+
 
 class DWG:
         'Common Base Class for all Equipment'
@@ -42,30 +44,67 @@ class DWG:
             out = []
             for n in names:
                 v = getattr(self, n)
-                out.append("{name:<30} : {value:>30}\n".format(name=n, value=v))
-            out.append('{}\n'.format('*' * 63))
-
+                out.append(v)
+                  
             return ''.join(out)
             
-        def DisplayEquipment(self):
+        def DisplayDWG(self):
             print str(self)
 
 
 
-def get_data(myfile):
+def get_csv(myfile):
+    global Heading
     i=0
     with open(myfile, 'rb') as csvfile:
         FileReader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in FileReader:
             if i == 0:
-                Heading = row
+                Heading=row
+                i=1
             else:
                 if (len(row[0]) > 2):
-                    DWG_List.append(Equipment(row[0], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[10], row[11]))
+                    DWG_List.append(DWG(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11]))
                 else:
                     pass
-            i = i+1
 
+
+def push_csv(mylist):
+    global Workbook_FileName
+    Workbook_FileName = 'DWG Porperties[{:%Y-%m-%d_%H%M%S}].csv'.format(DT.datetime.now())
+    
+    with open(Workbook_FileName, 'wb') as csvfile:
+        FileWriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+        
+        FileWriter.writerow(Heading)
+        for row in mylist:
+             #print row
+            names = ('DWGNAM',
+                   'SEC',
+                   'SUBSEC',
+                   'SH',
+                   'SHDWGNAM',
+                   'IEC_P',
+                   'IEC_I',
+                   'IEC_L',
+                   'DWGDESC1',
+                   'DWGDESC2',
+                   'DWGDESC3',
+                   'FULLFILENAME',)
+            out = []
+            for n in names:
+                v = getattr(row, n)
+                out.append(v)
+            #print out
+            FileWriter.writerow(out)
+            
+def display_csv(mylist):
+    for each in mylist:
+        print each
+
+def get_SHDWGNAM(mylist):
+    for each in mylist:
+        each.SHDWGNAM = each.DWGNAM.replace("D", "").replace("_", "")
 
 def main():
 
@@ -73,8 +112,15 @@ def main():
     parser.add_argument('DWG_File', help='DWG Properties File Name (.xls)')
     args = parser.parse_args()
 
-    get_data(args.DWG_File)
+    get_csv(args.DWG_File)
+    get_SHDWGNAM(DWG_List)
+    push_csv(DWG_List)
+
+
+    print "\n...Complete \n\n{!s} Generated".format(Workbook_FileName)
     
+
 if __name__ == '__main__':
     main()
     sys.exit()
+
